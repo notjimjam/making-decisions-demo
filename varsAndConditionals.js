6f07cb9dd1c8f5d45e44acaef980d29f6f576d08
Jamie Smith <thejamiemsmith@gmail.com>
2022-02-09 13:00:03 -0700

    if else complete

--- varsAndConditionals.js
+++ varsAndConditionals.js
@@ -1,5 +1,35 @@
 /*
     In this file, we'll be using variables, conditionals,
-    and loops to play out a fight between Jon Snow and
-    Jamie Lannister.
+    and loops to play out a fight between Eowyn and
+    Witch King of Angmar.
 */ 
+
+let eowynAttack = 50;
+let witchKingAttack = 45;
+
+if (eowynAttack > witchKingAttack) {
+    console.log("Eowyn is stronger.");
+} else if (witchKingAttack > eowynAttack){
+    console.log("Witch King is stronger");
+} else if (eowynAttack === witchKingAttack) {
+    console.log("They are the same strength");
+};
+
+let eowynHealth = 100;
+let eowynDefense = 0;
+
+if (eowynHealth + eowynDefense <= witchKingAttack) {
+    console.log("She dead");
+} else {
+   eowynHealth -= witchKingAttack;
+    console.log("She alive")
+    console.log(`She is down to ${eowynHealth} health points.`);
+};
+
+let coinLandsHeads = false;
+
+if (coinLandsHeads === true) {
+    console.log("Witch King gets to run away")
+} else {
+    console.log("Witch King got stabbed in the face")
+};
